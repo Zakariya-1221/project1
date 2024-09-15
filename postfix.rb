6d6e431958759b5e6af2130138f1dbe45352ffd2
Iram <masood.39@osu.edu>
2024-09-15 19:25:56 -0400

    Start of postfix method

---
+++ postfix.rb
@@ -0,0 +1,5 @@
+# A program to convert an infix expression to postfix expression
+# Evaluate arithmetic expressions by implementing your own methods to handle 
+# basic arithmetic operations and respect the order of operations (operator 
+# precedence). Operations include *, / (handle division by zero), %, +, -, and =
+
