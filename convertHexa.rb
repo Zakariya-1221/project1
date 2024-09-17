cda8178801cf15589972969e3a9641f8b93aee7a
Suvan Dommeti <suvandommeti0@gmail.com>
2024-09-17 18:46:07 -0400

    Added tests to conversions

--- convertHexa.rb
+++ convertHexa.rb
@@ -3,3 +3,7 @@ def convert_to_hexadecimal(decimal)
     puts "Decimal: #{decimal}"
     puts "Hexadecimal: #{hexadecimal}"
 end
+
+puts convert_to_hexadecimal(42)    # Expected: Hexadecimal: 2a
+puts convert_to_hexadecimal(255)   # Expected: Hexadecimal: ff
+puts convert_to_hexadecimal(1000)  # Expected: Hexadecimal: 3e8
--- convertOctal.rb
+++ convertOctal.rb
@@ -3,3 +3,7 @@ def convert_to_octal(decimal)
     puts "Decimal: #{decimal}"
     puts "Octal: #{octal}"
 end
+
+puts convert_to_octal(42)    # Expected: Octal: 52
+puts convert_to_octal(255)   # Expected: Octal: 377
+puts convert_to_octal(1000)  # Expected: Octal: 1750
